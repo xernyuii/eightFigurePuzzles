d7577296d04c4c899d4eeed529642e069fcab014
xernyuii <hfutxc.xern@gmail.com>
2017-03-08 20:36:29 +0800

    add

--- main.cpp
+++ main.cpp
@@ -14,10 +14,20 @@ typedef int statusArray[4][4];
 class Board{
 public:
 	int status,F,G,H;
+	statusArray arr;
+	int cid, pid;
+	Board(const statusArray& arr, const int F, const int G, const int H, const int cid, const int pid){
+		memcpy(this->arr, arr, sizeof(statusArray));
+		this->F = F;
+		this->G = G;
+		this->H = H;
+		this->cid = cid;
+		this->pid = pid;
+	}
 	bool operator<(const Board &b) const {
 		return F < b.F;
 	}
-	bool operator<=(const Board &b) const {
+	bool operator<=(const Board &b) const { 
 		return F <= b.F;
 	}
 	bool operator==(const Board &b) const{
@@ -33,8 +43,8 @@ std::set<int> closed;
 int DIS[10][10];
 statusArray START;
 statusArray END;
-
-
+int movx[] = {-1, 0, 1, 0};
+int movy[] = {0, 1, 0, -1};
 void int2array(int num, statusArray& arr){
 	for(int i=1; i<=3; i++){
 		for(int j=1; j<=3; j++){
@@ -43,16 +53,16 @@ void int2array(int num, statusArray& arr){
 		}
 	}
 }
-void array2int(statusArray& arr, int& num){
-	num = 0;
+int array2int(statusArray& arr){
+	int num = 0;
 	for(int i=1; i<=3; i++){
 		for(int j=1; j<=3; j++){
 			num *= 10;
 			num += arr[i][j];
 		}
 	}
+	return num;
 }
-
 
 void preWorkforDIS(){
 
@@ -108,6 +118,38 @@ bool check(const statusArray& now, const statusArray& end){
 	return (cnt_now % 2) == (cnt_end % 2);
 }
 
+int solve(const statusArray& start, const statusArray& end){
+	opening.clear();
+	closed.clear();
+	path.clear();
+	int id = 0;
+	if(check(start, end)) return -1;
+	opening.insert(Board(start, getH(start, end), 0, getH(start, end), id, id++));
+	while(opening.size()){
+		Board p = *(opening.begin());
+		opening.erase(opening.begin());
+		path.push_back(p);
+		closed.insert(array2int(p.arr));
+		if(success(p.arr, end)) return p.G;
+		int cx = 0, cy = 0;
+		for(int i=1; i<=3; i++){
+			if(cx != 0) break;
+			for(int j=1; j<=3; j++){
+				if(p.arr[i][j] == 0){
+					cx = i;
+					cy = j;
+					break;
+				}
+			}
+		}
+
+
+
+	}
+
+
+
+}
 
 
 
